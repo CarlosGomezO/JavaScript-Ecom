a7ae514c1009c14c16571ec16977e84adea1b74e
Carlos Gomez <carlosmgo94@gmail.com>
2023-05-21 21:41:15 -0300

    Segunda entrega

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// Carga de datos con flujo y condicionales
+/* Carga de datos con flujo y condicionales /*
+ 
 
 /*
 let edad = parseInt(prompt("Ingrese edad"));
@@ -77,25 +78,247 @@ if (cuotas === 12 ){
 //CALCULADORA FUNCION
 
 
-let numero1 = parseInt(prompt("Ingrese numb 1"));
-let numero2 = parseInt(prompt("Ingrese numb 2"));
-let operacion = prompt("Ingrese operación");
+//let numero1 = parseInt(prompt("Ingrese numb 1"));
+//let numero1 = parseInt(prompt("Ingrese numb 1"));
 
-function calculadora(a,b,c){
-  resultado = 0
-  if (c === "+"){
-    resultado=a+b
-  }
-  if (b === "-"){
-    resultado= a-b
-  }
-  if (c === "*"){
-    resultado= a*c
-  }
-  if (d === "/"){
-    resultado= a/d
-  }
+//
+//function calculadora(a,b,c){
+//
+//  resultado = 0
+//  if (c === "+"){
+//    resultado=a+b
+//  }
+//  if (c === "-"){
+//    resultado= a-b
+//  }
+//  if (c === "*"){
+//    resultado= a*b
+//  }
+//  if (c === "/"){
+//    resultado= a/b
+//  }
+//}
+
+//calculadora(numero1, numero2, operacion);
+//alert(" El resultado de" + " " + numero1 + operacion + numero2 + "=" + resultado);
+
+
+
+
+
+/*
+// OBJETOS
+
+const persona1 = {
+  nombre: "pepito",
+  edad: 45,
+  direccion: "peña"
+};
+
+
+
+
+let clave = "edad";
+
+console.log(persona1[clave]) // Se llama al valor de clave ("edad") en persona1 desde una variable fuera del objeto
+
+console.log(persona1.nombre);
+console.log(persona1.edad);
+console.log(persona1.direccion);
+
+
+
+// CREACION DE OBJETOS MEDIANTE UNA FUNCION ****
+
+function Persona(nombrePersona, edadPersona, direccionPersona){
+  this.nombre = nombrePersona;
+  this.edad = edadPersona;
+  this.direccion = direccionPersona
 }
 
-calculadora(numero1, numero2, operacion);
-alert(" El resultado de" + " " + numero1 + operacion + numero2 + "=" + resultado);
+const persona2 = new Persona("Carlos", 30, "Coronel Díaz");
+const persona3 = new Persona ("Mica", 29, "Villa Urquiza" );
+
+console.log(persona2);
+console.log(persona3);
+
+
+
+// Método más efectivo
+// Para muchos parametros 
+
+function Personas(info){
+  this.nombre = info.nombre;
+  this.edad = info.edad;
+  this.direccion = info.direccion
+}
+
+const persona4 = new Personas({
+  nombre: "Gomez",
+  edad: 35,
+  direccion:"Las Heras"
+});
+console.log(persona4);
+console.log(persona4.nombre);
+console.log(persona4.direccion);
+
+
+
+// CREAR OBJETO COMO PRODUCTO
+
+function ProductoML(nombre, precio, imagen){
+  this.nombre = nombre;
+  this.precio = precio;
+  this.imagen = imagen;
+  
+};
+const producto1 = new ProductoML ("Monitor", 295319, "htpps.mckmosvnjd");
+console.log(producto1.nombre);
+
+for (const i in ProductoML){
+  console.log("clave", i);
+  console.log("valor", ProductoML[i]);
+}
+
+*/
+
+/*
+ const operaciones = (op) => {
+   if (op === "sumar") {
+     return (a, b) => a + b;
+   } else if (op === "restar") {
+     return (a, b) => a - b;
+  } else if (op === "multiplicar") {
+     return (a, b) => a * b;
+   } else if (op === "dividir") {
+     return (a, b) => a / b;
+   }
+ };
+
+
+
+
+
+let operacion = prompt("Ingrese el nombre de la operacion deseada");
+const calcular = operaciones(operacion);
+let num1 = parseInt(prompt("Ingrese el numero"));
+let num2 = parseInt(prompt("Ingrese el numero"));
+let mensaje = `El resultado de la operacion es: ${calcular(num1, num2)}`;
+
+
+alert(mensaje);
+*/
+
+
+
+
+
+
+
+
+
+
+// **************************************** SEGUNDA ENTREGA *********************************************************************************
+
+
+
+function ProductoML(id,nombre, precio, imagen, stock){
+  this.nombre = nombre;
+  this.precio = precio;
+  this.imagen = imagen;
+  this.stock = stock 
+};
+
+const productos = [
+  producto1 = new ProductoML (012,"tabla", 800, "https.mckmosvnjd", 10),
+  producto2 = new ProductoML (022,"tabla 1", 900, "https.mckmosvnjd", 10),
+  producto3 = new ProductoML (032,"tabla 2", 1000, "https.mckmosvnjd", 10),
+  producto4 = new ProductoML (042,"tabla 3", 2000, "https.mckmosvnjd", 10),
+]
+
+/*
+
+// MUESTRA OBJETOS con FOR
+//for (const i in productos){
+//  console.log("valor", productos[i]);
+//}
+
+//FOR EACH
+//productos.forEach((item) => {
+//  console.log(item);
+//});
+
+// BUSQUEDA POR CONDICIÓN
+const encontrados = productos.filter((item) => item.precio >= 1000);
+console.log(encontrados);
+
+
+// BUSQUEDA CON "FIND"
+
+let nombre = prompt("ingrese el nombre del producto a buscar");
+const encontrado = productos.find((item) => item.nombre === nombre);
+
+if (encontrado) {
+  let mensaje = `
+    Nombre: ${encontrado.nombre}
+    Precio: $${encontrado.precio}
+  `;
+
+  alert(mensaje);
+} else {
+  alert("Producto no encontrado");
+}
+
+//BUSQUEDA CON "FILTER"
+
+let precio = parseInt(prompt("Ingrese el precio minimo del producto"));
+const busqueda = productos.filter((item) => item.precio > precio && item.nombre.includes("a"));
+
+busqueda.forEach((item) => {
+  let mensaje = `
+    ID: ${item.id}
+    Nombre: ${item.nombre}
+    Precio: $${item.precio}
+  `;
+
+    alert(mensaje);
+});
+
+*/
+
+
+// CALCULAR OPERACIONES DE SUMA, RESTA MULTIPLICACION Y DIVISIÓN sobre los precios del arreglo 
+
+const operaciones = (op) => {
+  if (op === "+") {
+    return (a, b) => a + b;
+  } else if (op === "-") {
+    return (a, b) => a - b;
+ } else if (op === "*") {
+    return (a, b) => a * b;
+  } else if (op === "/") {
+    return (a, b) => a / b;
+  }
+};
+
+//let operacion = prompt("Ingresá +, -, * ó /");
+let operacion = "+";
+//let numeroProducto = parseInt(prompt("Ingresá un numero"));
+let numeroProducto = 50;
+const calcular = operaciones(operacion);
+
+const info = productos.map((item) =>{
+  return {
+    nombre: item.nombre,
+    precio: (`Precio antiguo ${item.precio} `),
+    precio: (`nuevo precio ${calcular(item.precio,numeroProducto)}`)
+  }
+});
+console.log(info);
+
+console.log(`lista de productos alterada por consola = ${info}`); // NOTA: POR "ALERT" ARROJA = [object Object],[object Object],[object Object],[object Object]
+
+
+
+
+//*************************************** HASTA ACA ES LA 2DA ENTREGA *************************************************************************
